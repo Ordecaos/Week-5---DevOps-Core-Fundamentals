301b2e31eb301750c2724c2cf8eb63d8cbc87eae
Ordecaos <jackwatkinson1996@hotmail.co.uk>
2021-11-08 16:51:29 +0000

    Fixed spelling mistake in Models

--- application/models.py
+++ application/models.py
@@ -1,24 +1,24 @@
 from application import db
 
 class Players(db.Model):
-    Player_ID = db.Column(db.Interger, primary_key = True)
+    Player_ID = db.Column(db.Integer, primary_key = True)
     Player_name = db.Column(db.String(255))
     Character_Name = db.Column(db.String(255))
-    Character_Level = db.Column(db.Interger(2))
+    Character_Level = db.Column(db.Integer(2))
     Character_Class = db.Column(db.String(255))
     Character_Race = db.Column(db.String(255))
-    Campaign = db.Column(db.Interger,  db.ForeignKeys == 'campaigns.Campaign_ID')
+    Campaign = db.Column(db.Integer,  db.ForeignKeys == 'campaigns.Campaign_ID')
 
 class Campaigns(db.Model):
-    Campaign_ID = db.Column(db.Interger, primary_key = True)
+    Campaign_ID = db.Column(db.Integer, primary_key = True)
     Campaign_Name = db.Column(db.String(255))
     Campaign_Setting = db.Column(db.String(255))
-    No_of_Players = db.Column(db.Interger)
-    Books_Used = db.Column(db.Interger, db.ForeignKeys == 'books.Book_ID')
+    No_of_Players = db.Column(db.Integer)
+    Books_Used = db.Column(db.Integer, db.ForeignKeys == 'books.Book_ID')
     Players = db.relationship('Players', backref='campaign')
 
 class Books(db.Model):
-    Book_ID = db.Column(db.Interger, primary_key = True)
+    Book_ID = db.Column(db.Integer, primary_key = True)
     Book_Name = db.Column(db.String(255))
     Book_Contents = db.Column(db.String(255))
     Campaigns = db.relationship('Campaigns', backref='book')
