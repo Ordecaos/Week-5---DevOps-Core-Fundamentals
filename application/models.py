6ddcb164380b36de93bcb572ea41c8e34ecb773f
Ordecaos <jackwatkinson1996@hotmail.co.uk>
2021-11-08 16:47:16 +0000

    Fixed a Logical Error

--- application/models.py
+++ application/models.py
@@ -7,14 +7,14 @@ class Players(db.Model):
     Character_Level = db.Column(db.Interger(2))
     Character_Class = db.Column(db.String(255))
     Character_Race = db.Column(db.String(255))
-    Campaign = db.Column(db.Interger,  db.ForeignKeys = 'campaigns.Campaign_ID')
+    Campaign = db.Column(db.Interger,  db.ForeignKeys == 'campaigns.Campaign_ID')
 
 class Campaigns(db.Model):
     Campaign_ID = db.Column(db.Interger, primary_key = True)
     Campaign_Name = db.Column(db.String(255))
     Campaign_Setting = db.Column(db.String(255))
     No_of_Players = db.Column(db.Interger)
-    Books_Used = db.Column(db.Interger, db.ForeignKeys = 'books.Book_ID')
+    Books_Used = db.Column(db.Interger, db.ForeignKeys == 'books.Book_ID')
     Players = db.relationship('Players', backref='campaign')
 
 class Books(db.Model):
