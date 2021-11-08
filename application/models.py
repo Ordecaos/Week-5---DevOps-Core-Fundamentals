de7808cea92a3cbeb36bf5ebb4c5b5add536d59a
Ordecaos <jackwatkinson1996@hotmail.co.uk>
2021-11-08 16:57:41 +0000

    Updated Models again due to incorrectly named attribute

--- application/models.py
+++ application/models.py
@@ -4,17 +4,17 @@ class Players(db.Model):
     Player_ID = db.Column(db.Integer, primary_key = True)
     Player_name = db.Column(db.String(255))
     Character_Name = db.Column(db.String(255))
-    Character_Level = db.Column(db.Integer(2))
+    Character_Level = db.Column(db.Integer)
     Character_Class = db.Column(db.String(255))
     Character_Race = db.Column(db.String(255))
-    Campaign = db.Column(db.Integer,  db.ForeignKeys == 'campaigns.Campaign_ID')
+    Campaign = db.Column(db.Integer, db.ForeignKey == 'campaigns.Campaign_ID')
 
 class Campaigns(db.Model):
     Campaign_ID = db.Column(db.Integer, primary_key = True)
     Campaign_Name = db.Column(db.String(255))
     Campaign_Setting = db.Column(db.String(255))
     No_of_Players = db.Column(db.Integer)
-    Books_Used = db.Column(db.Integer, db.ForeignKeys == 'books.Book_ID')
+    Books_Used = db.Column(db.Integer, db.ForeignKey == 'books.Book_ID')
     Players = db.relationship('Players', backref='campaign')
 
 class Books(db.Model):
